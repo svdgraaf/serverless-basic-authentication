7bcd68e4040ce83a0fc84e18176a95702322f987
Sander van de Graaf <mail@svdgraaf.nl>
2019-01-21 12:24:39 +0100

    Make ES linter happy

--- index.js
+++ index.js
@@ -1,109 +1,101 @@
-'use strict'
 const fs = require('fs');
 const chalk = require('chalk');
 
-
-class SetupBasicAuthentication {
-  constructor (serverless, options) {
+module.exports = class SetupBasicAuthentication {
+  constructor(serverless, options) {
+    this.options = options;
+    this.serverless = serverless;
 
     // add the basic authentication function to the functions as soon as possible
-    injectBasicAuthFunction(serverless);
+    this.injectBasicAuthFunction(serverless);
 
     this.hooks = {
-      'before:package:initialize': function () {
-        // add our custom authenticator
-        addAuthFileToPackage(serverless);
+      'before:package:initialize': this.addAuthorizer.bind(this),
+      'after:package:createDeploymentArtifacts': this.removeAuthorizer.bind(this),
+      'before:deploy:deploy': this.configureApiGatewayKeySource.bind(this),
+    };
+  }
 
-        addAuthorizerFunctionToPrivateFunctions(serverless);
-      },
-      'after:package:createDeploymentArtifacts': function () {
-        // remove the custom authenticator
-        removeFileFromPackage(serverless)
-      },
-      'before:deploy:deploy': function() {
-        // // add the basic authenticator function
-        // injectBasicAuthFunction(serverless);
+  addAuthorizer() {
+    // add our custom authenticator
+    this.addAuthFileToPackage();
 
-        // configure api gateway to check for the right place for the key
-        configureApiGatewayKeySource(serverless);
+    this.addAuthorizerFunctionToPrivateFunctions();
+  }
+
+  removeAuthorizer() {
+    this.serverless.cli.consoleLog(`Basic Authentication: ${chalk.yellow('Removing Symlink for Basic Authenticator')}`);
+    fs.unlinkSync(`${this.serverless.config.servicePath}/basic_auth.py`);
+  }
+
+  addAuthFileToPackage() {
+    if (!this.serverless.package) {
+      this.serverless.package = {};
+    }
+
+    if (!this.serverless.package.include) {
+      this.serverless.package.include = [];
+    }
+
+    this.serverless.cli.consoleLog(`Basic Authentication: ${chalk.yellow('Adding Symlink for Basic Authenticator')}`);
+    // @TODO: Make target filename randomized with something, to prevent overriding
+    // any files
+
+    // append our auth.py file to the package
+    this.serverless.package.include.push(`${__dirname}/auth.py`);
+    fs.symlinkSync(`${__dirname}/basic_auth.py`, `${this.serverless.config.servicePath}/basic_auth.py`);
+  }
+
+  injectBasicAuthFunction() {
+    this.serverless.cli.consoleLog(`Basic Authentication: ${chalk.yellow('Adding function for Basic Authenticator')}`);
+    const basicAuthenticator = {
+      handler: 'basic_auth.basicAuth',
+      runtime: 'python3.6',
+    };
+
+    // add the basic authenticator function
+    this.serverless.service.functions.basicAuthenticator = basicAuthenticator;
+  }
+
+  addAuthorizerFunctionToPrivateFunctions() {
+    // for each function which is marked as 'private', set the basic authenticator
+    // if it doesn't have a custom authenticator yet
+    Object.keys(this.serverless.service.functions).each((functionName) => {
+      // ignore our own function
+      if (functionName === 'basicAuthenticator') {
+        return;
       }
-    }
-  }
-}
 
-function removeFileFromPackage(serverless) {
-  serverless.cli.consoleLog('Basic Authentication: ' + chalk.yellow('Removing Symlink for Basic Authenticator'));
-  fs.unlinkSync(serverless.config.servicePath + "/basic_auth.py")
-}
+      // get all function configs
+      const fnctn = this.serverless.service.functions[functionName];
 
-function addAuthFileToPackage(serverless) {
-  if(!serverless.package) {
-    serverless.package = {}
-  }
-  if(!serverless.package.include) {
-    serverless.package.include = []
-  }
-
-  serverless.cli.consoleLog('Basic Authentication: ' + chalk.yellow('Adding Symlink for Basic Authenticator'));
-  // @TODO: Make target filename randomized with something, to prevent overriding
-  // any files
-
-  // append our auth.py file to the package
-  serverless.package.include.push(__dirname + "/auth.py")
-  fs.symlinkSync(__dirname + "/basic_auth.py", serverless.config.servicePath + "/basic_auth.py")
-}
-
-function injectBasicAuthFunction (serverless) {
-  serverless.cli.consoleLog('Basic Authentication: ' + chalk.yellow('Adding function for Basic Authenticator'));
-  var basicAuthenticator = {
-    handler: 'basic_auth.basicAuth',
-    runtime: 'python3.6'
-  }
-
-  // add the basic authenticator function
-  serverless.service.functions.basicAuthenticator = basicAuthenticator;
-}
-
-function addAuthorizerFunctionToPrivateFunctions(serverless) {
-  // for each function which is marked as 'private', set the basic authenticator
-  // if it doesn't have a custom authenticator yet
-  for(let function_name in serverless.service.functions) {
-
-    // ignore our own function
-    if(function_name == 'basicAuthenticator') {
-      continue;
-    }
-
-    var fnctn = serverless.service.functions[function_name];
-
-    // check if any of the http events is marked as private, and if that event
-    // also doesn't have a custom authorizer already, apply our authenticator
-    for(let fnctn_event in fnctn['events']) {
-      if(
-        serverless.service.functions[function_name].events[fnctn_event].http != null &&
-        serverless.service.functions[function_name].events[fnctn_event].http.private == true &&
-        serverless.service.functions[function_name].events[fnctn_event].http.authorizer == null
-      ) {
-        serverless.service.functions[function_name].events[fnctn_event].http.authorizer = {
-          name: 'basicAuthenticator',
-          identitySource: '', // this is only valid if we set cache ttl to 0
-          resultTtlInSeconds: 0,
-          type: 'REQUEST'
-
+      // check if any of the http events is marked as private, and if that event
+      // also doesn't have a custom authorizer already, apply our authenticator
+      Object.keys(fnctn.events).each((fnctnEvent) => {
+        if (
+          this.serverless.service.functions[functionName].events[fnctnEvent].http != null
+          && this.serverless.service.functions[functionName].events[fnctnEvent].http.private === true
+          && this.serverless.service.functions[functionName].events[fnctnEvent].http.authorizer == null
+        ) {
+          this.serverless.service.functions[functionName].events[fnctnEvent].http.authorizer = {
+            name: 'basicAuthenticator',
+            identitySource: '', // this is only valid if we set cache ttl to 0
+            resultTtlInSeconds: 0,
+            type: 'REQUEST',
+          };
+          this.serverless.cli.consoleLog(`Basic Authentication: ${chalk.yellow(`Enabled for ${functionName}`)}`);
         }
-        serverless.cli.consoleLog('Basic Authentication: ' + chalk.yellow('Enabled for ' + function_name));
-      }
+      });
+    });
+  }
+
+  configureApiGatewayKeySource() {
+    const template = this.serverless.service.provider.compiledCloudFormationTemplate;
+    if (template.Resources.ApiGatewayRestApi != null) {
+      this.serverless.cli.consoleLog(
+        `Basic Authentication: ${chalk.yellow('Configuring Api Gateway for Basic Authenticator')}`,
+      );
+      template.Resources.ApiGatewayRestApi.Properties.ApiKeySourceType = 'AUTHORIZER';
     }
   }
-}
-
-function configureApiGatewayKeySource(serverless) {
-  var template = serverless.service.provider.compiledCloudFormationTemplate;
-  if(template.Resources.ApiGatewayRestApi != null) {
-    serverless.cli.consoleLog('Basic Authentication: ' + chalk.yellow('Configuring Api Gateway for Basic Authenticator'));
-    template.Resources.ApiGatewayRestApi.Properties.ApiKeySourceType = 'AUTHORIZER'
-  }
-}
-
-// now we need to make our plugin object available to the framework to execute
-module.exports = SetupBasicAuthentication
+};
